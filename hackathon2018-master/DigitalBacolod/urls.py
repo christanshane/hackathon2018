8ff761b9a8cf23190445dd9809d8a4891875d4f4
mauiblade12 <43491641+mauiblade12@users.noreply.github.com>
2018-09-23 03:33:32 +0800

    Add files via upload

--- hackathon2018-master/DigitalBacolod/urls.py
+++ hackathon2018-master/DigitalBacolod/urls.py
@@ -1,23 +1,23 @@
-"""DigitalBacolod URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from dbapp import views as dbapp
+from django.contrib.auth import views as auth
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dbapp.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    ################### DASHBOARD URLS ###############
+
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', dbapp.index, name='home'),
+    url(r'^orders$', dbapp.index, name='home'),
+    url(r'^order/(?P<order_id>\d+)/$', dbapp.show, name='show'),
+    url(r'^order/new/$', dbapp.new, name='new'),
+    url(r'^order/edit/(?P<order_id>\d+)/$', dbapp.edit, name='edit'),
+    url(r'^order/delete/(?P<order_id>\d+)/$', dbapp.destroy, name='delete'),
+    url(r'^users/login/$', auth.LoginView.as_view(template_name="login.html"), name="login"),
+    url(r'^users/logout/$', auth.LogoutView.as_view, {'next_page': '/login'}, name='logout'),
 ]
